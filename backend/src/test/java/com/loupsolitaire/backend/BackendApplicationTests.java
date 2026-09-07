d080f53f83716af9dde4d4cd87e5426457b9b7bc
Marius <siwertzm@gmail.com>
2026-09-07 18:44:14 +0200

    test sonar

--- backend/src/test/java/com/loupsolitaire/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/loupsolitaire/backend/BackendApplicationTests.java
@@ -8,10 +8,10 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {
         "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
         "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
         "spring.jpa.hibernate.ddl-auto=create-drop",
         "jwt.secret=dGVzdF9zZWNyZXRfa2V5X2Zvcl91bml0X3Rlc3RzX29ubHlfMzJi",
-        "jwt.refresh-expiration-days=30"
+        "jwt.refresh-expiration-days=30",
+        "spring.datasource.hikari.connection-init-sql="
 })
 class BackendApplicationTests {
 
