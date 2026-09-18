4c28d88fcf2e3ef4f7fde41e703a7c770f614ace
Marius <siwertzm@gmail.com>
2026-09-18 17:48:52 +0200

    modification texte disciplin/register hauteur

---
+++ backend/src/main/resources/db/changelog/changes/003-discipline-description.sql
@@ -0,0 +1,7 @@
+UPDATE public.discipline
+SET description = 'Les Maîtres des Ténèbres et les nombreuses créatures malfaisantes qui leur obéissent ont la faculté de vous porter atteinte en faisant usage de leur force psychique. La technique du Bouclier psychique vous permet cependant de ne pas perdre de points d''ENDURANCE lorsque vous vous trouvez soumis à une telle agression.'
+WHERE id = 'BOUCLIER_PSYCHIQUE';
+
+UPDATE public.discipline
+SET description = 'Cette technique permet au Seigneur Kaï d''attaquer un ennemi en se servant de la force de son esprit : on peut l''utiliser en même temps qu''une arme de combat habituelle et l''on dispose alors de 2 points supplémentaires d''HABILITÉ. Cette puissance psychique, cependant, n''est pas forcément efficace avec toutes les créatures: il se peut que certaines d''entre elles y soient insensibles. Si le cas se présente, vous en serez averti au cours de votre mission.'
+WHERE id = 'PUISSANCE_PSYCHIQUE';
