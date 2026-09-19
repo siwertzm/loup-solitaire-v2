0630aa58cc9709751047d1794e42fdfb5483766f
Marius <siwertzm@gmail.com>
2026-09-19 20:06:02 +0200

    lien mort

---
+++ backend/src/main/resources/db/changelog/changes/004-lien-mort.sql
@@ -0,0 +1,9 @@
+update public.lien
+set chapitre_cible_id = '46'
+where chapitre_id = '2102'
+	and chapitre_cible_id ='172';
+
+update public.lien
+set chapitre_cible_id = '144'
+where chapitre_id = '108'
+	and chapitre_cible_id ='129';
