e24082a227ada196de2537c118d7df3db2386ea5
Marius <siwertzm@gmail.com>
2026-09-20 20:43:17 +0200

    android bar

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.loupsolitaire.app',
   appName: 'Loup Solitaire',
-  webDir: 'dist/loup-solitaire-front/browser'
+  webDir: 'dist/loup-solitaire-front/browser',
+  backgroundColor: '#2c3323',
+  plugins: {
+    SystemBars: {
+      style: 'DARK',
+      initialViewportFitValueHint: 'cover'
+    }
+  }
 };
 
 export default config;
